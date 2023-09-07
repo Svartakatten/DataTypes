0911e9ac7c7f34a26d003f312e81cbe9e5d49894
Svartakatten <rikexhx@gmail.com>
2023-09-07 13:42:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Tobias";
+            //// Värdetyp
+            //int x = 15;
+            //int y = x;
+            //x = 30;
+            //Console.WriteLine(y);
+
+            //// Referenstyper
+            //StringBuilder strb1 = new StringBuilder("Hej");
+            //StringBuilder strb2 = strb1;
+            //strb1.AppendLine();
+            //strb1.Append("Hur mår du?");
+
+            //Console.WriteLine(strb2.ToString());
+        }
+    }
+}
