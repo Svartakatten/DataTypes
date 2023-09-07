9e1843c0e213f66691efa9f3721d98b1b5f7aebb
Svartakatten <rikexhx@gmail.com>
2023-09-07 15:16:53 +0200

    Lite små ändringar

--- Program.cs
+++ Program.cs
@@ -20,6 +20,40 @@ namespace DataTypes
             //strb1.Append("Hur mår du?");
 
             //Console.WriteLine(strb2.ToString());
+
+            //Console.WriteLine("Signerad heltal");
+            //Console.WriteLine($"int {int.MinValue} till {int.MaxValue}");
+            //Console.WriteLine("Usignerad heltal");
+            //Console.WriteLine($"uint {uint.MinValue} till {uint.MaxValue}");
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //int a = int.MaxValue;
+            //int b = a + 1;
+            //Console.WriteLine(b);
+
+            //bool svar = true;
+            //svar = false;
+            //Console.WriteLine(svar);
+            //string Name = "Reidar";
+            //Console.WriteLine(Name);
+            int a1 = 20;
+            //int b1 = 4;
+            if (a1 > 20)
+            {
+                //Min kod
+                Console.WriteLine("större");
+
+            }
+            else if (a1 > 20)
+            {
+                //Min kod
+                Console.WriteLine("mindre");
+            }
+            else
+            {
+                Console.WriteLine("lika");
+
+            }
         }
     }
 }
